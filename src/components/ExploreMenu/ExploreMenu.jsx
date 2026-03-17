e671dec0ed07bc5c98e84935f6a632f00f5195a0
Daivat Dhimmar <121424784+daivat9228@users.noreply.github.com>
2026-03-17 12:48:20 +0530

    Refactor ExploreMenu to use context for menu_list

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,7 +1,10 @@
-import React from "react";
-import { menu_list } from "../../assets/assets"; 
+import React, { useContext } from "react";
+import { storeContext } from "../../context/StoreContext";
 
 const ExploreMenu = ({ category, setCategory }) => {
+
+  const { menu_list } = useContext(storeContext);
+  
   return (
     <div
       id="explore-menu"
